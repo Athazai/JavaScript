bd6cb95f6ec7d49fd547b33636140f47b4ef639b
Athallah <93963320+Athazai@users.noreply.github.com>
2022-01-12 13:34:53 +0700

    Add files via upload

---
+++ object.js
@@ -0,0 +1,19 @@
+// const family = {
+// 	NamaBokap: 'Hendrik Christianto',
+// 	NamaNyokap: 'Army Poernomo',
+// 	NamaAnakPertama: 'Athallah Z.A',
+// 	NamaAnakkedua: 'Dayyan E.A',
+
+// };
+// console.log(family)
+
+
+const Restaurant = {
+    nameRestaurant: 'AW',
+    cityRestaurant: 'Bogor',
+    provinceRestaurant: 'Jawa Barat',
+    favoritedrink: 'Milkshake Vanilla',
+    favoritefood: 'Chiken Wings',
+    isVegan: true
+};
+console.log(Restaurant)
