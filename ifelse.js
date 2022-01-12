827040ed370aac3775353dd72fffc435f49042b5
Athazai <athallahzaidan90@gmail.com>
2022-01-12 13:28:20 +0700

    first commit

---
+++ ifelse.js
@@ -0,0 +1,31 @@
+// let skl = 100;
+
+// if (skl > 75) {
+//     console.log("pulang")
+// }
+// else {
+//     console.log("Tidak Pulang")
+// }
+
+
+//logika nilai
+
+let nilai = 70;
+
+if (nilai >= 90) {
+    console.log("Selamat anda mendapatkan Nilai A")
+} else {
+    if (nilai >= 80) {
+        console.log("Selamat anda mendapatkan Nilai B")
+    } else {
+        if (nilai >= 70) {
+            console.log("Selamat anda mendapatkan Nilai C")
+        } else {
+            if (nilai >= 60) {
+                console.log("Selamat anda mendapatkan Nilai D")
+            } else {
+                console.log("GAGAL")
+            }
+        }
+    }
+}
